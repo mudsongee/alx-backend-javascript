018d63bcefbda083badda2af5c9bfc6dd112f022
mudsongee <mohdahmedjafar@gmail.com>
2023-11-28 02:18:53 +0100

    ES6_basic

--- 0x00-ES6_basic/12-main.js
+++ 0x00-ES6_basic/12-main.js
@@ -1,5 +1,5 @@
 import createEmployeesObject from '/root/alx-backend-javascript/0x00-ES6_basic/11-createEmployeesObject.js';
-import createReportObject from '../12-createReportObject.js';
+import createReportObject from '/root/alx-backend-javascript/0x00-ES6_basic/12-createReportObject.js';
 
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
