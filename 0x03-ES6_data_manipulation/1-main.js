a256baa238f00b57dcb47971483201b34441cb28
mudsongee <mohdahmedjafar@gmail.com>
2023-12-05 22:19:08 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/1-main.js
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -1,2 +1,2 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
+import getListStudentIds from './1-get_list_student_ids.js';
+import getListStudents from './0-get_list_students.js';
